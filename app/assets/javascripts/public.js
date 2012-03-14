c2a46bdb179244dbc5cf1ac1448467cf3547e66a
jdfdesign <regis@joufdesign.com>
2012-03-14 15:11:20 -0430

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
 		$(window).bind('resize', resizeContent); 
 		$('.carousel').each(function(index) {
 			var _self = $(this);
-			console.log(_self.find('.item').length);
 			if(_self.find('.item').length > 1) {
 				_self.carousel();
 			}
